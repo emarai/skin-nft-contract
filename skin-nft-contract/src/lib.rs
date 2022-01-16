ce224f78a0f0f7460d0ad6b517f8c2c35f3a3b2c
Irfianto <irfi@paras.id>
2022-01-16 18:45:41 +0700

    feat: remove treasury

--- skin-nft-contract/src/lib.rs
+++ skin-nft-contract/src/lib.rs
@@ -105,7 +105,6 @@ pub struct Contract {
     metadata: LazyOption<NFTContractMetadata>,
     // CUSTOM
 	token_series_by_id: UnorderedMap<TokenSeriesId, TokenSeries>,
-    treasury_id: AccountId,
 }
 
 const DATA_IMAGE_SVG_NEAR_ICON: &str = "data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 288 288'%3E%3Cg id='l' data-name='l'%3E%3Cpath d='M187.58,79.81l-30.1,44.69a3.2,3.2,0,0,0,4.75,4.2L191.86,103a1.2,1.2,0,0,1,2,.91v80.46a1.2,1.2,0,0,1-2.12.77L102.18,77.93A15.35,15.35,0,0,0,90.47,72.5H87.34A15.34,15.34,0,0,0,72,87.84V201.16A15.34,15.34,0,0,0,87.34,216.5h0a15.35,15.35,0,0,0,13.08-7.31l30.1-44.69a3.2,3.2,0,0,0-4.75-4.2L96.14,186a1.2,1.2,0,0,1-2-.91V104.61a1.2,1.2,0,0,1,2.12-.77l89.55,107.23a15.35,15.35,0,0,0,11.71,5.43h3.13A15.34,15.34,0,0,0,216,201.16V87.84A15.34,15.34,0,0,0,200.66,72.5h0A15.35,15.35,0,0,0,187.58,79.81Z'/%3E%3C/g%3E%3C/svg%3E";
@@ -126,10 +125,9 @@ enum StorageKey {
 #[near_bindgen]
 impl Contract {
     #[init]
-    pub fn new_default_meta(owner_id: ValidAccountId, treasury_id: ValidAccountId) -> Self {
+    pub fn new_default_meta(owner_id: ValidAccountId) -> Self {
         Self::new(
             owner_id,
-            treasury_id,
             NFTContractMetadata {
                 spec: NFT_METADATA_SPEC.to_string(),
                 name: "Skins Collectible".to_string(),
@@ -145,7 +143,6 @@ impl Contract {
     #[init]
     pub fn new(
         owner_id: ValidAccountId, 
-        treasury_id: ValidAccountId, 
         metadata: NFTContractMetadata
     ) -> Self {
         assert!(!env::state_exists(), "Already initialized");
@@ -160,20 +157,7 @@ impl Contract {
             ),
             token_series_by_id: UnorderedMap::new(StorageKey::TokenSeriesById),
             metadata: LazyOption::new(StorageKey::Metadata, Some(&metadata)),
-            treasury_id: treasury_id.to_string(),
         }
-    }
-
-    // Treasury
-    #[payable]
-    pub fn set_treasury(&mut self, treasury_id: ValidAccountId) {
-        assert_one_yocto();
-        assert_eq!(
-            env::predecessor_account_id(),
-            self.tokens.owner_id,
-            "Skins: Owner only"
-        );
-        self.treasury_id = treasury_id.to_string();
     }
 
     // CUSTOM
@@ -298,10 +282,7 @@ impl Contract {
         );
         let token_id: TokenId = self._nft_mint_series(token_series_id, receiver_id.to_string());
 
-        let for_treasury = price as u128 * TREASURY_FEE / 10_000u128;
-        let price_deducted = price - for_treasury;
-        Promise::new(token_series.creator_id).transfer(price_deducted);
-        Promise::new(self.treasury_id.clone()).transfer(for_treasury);
+        Promise::new(token_series.creator_id).transfer(price);
 
         refund_deposit(env::storage_usage() - initial_storage_usage, price);
 
