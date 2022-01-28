d3ceddc2b375ef009da9b579e26522b326510ec1
Irfi <irfi@paras.id>
2022-01-28 18:17:10 +0700

    get last token id by owner fuse

--- skin-nft-contract/src/lib.rs
+++ skin-nft-contract/src/lib.rs
@@ -7,7 +7,7 @@ use near_contract_standards::non_fungible_token::metadata::{
 use near_contract_standards::non_fungible_token::NonFungibleToken;
 use near_contract_standards::non_fungible_token::{Token, TokenId};
 use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
-use near_sdk::collections::{LazyOption, UnorderedMap, UnorderedSet};
+use near_sdk::collections::{LazyOption, LookupMap, UnorderedMap, UnorderedSet};
 use near_sdk::env::is_valid_account_id;
 use near_sdk::json_types::{ValidAccountId, U128, U64};
 use near_sdk::serde::{Deserialize, Serialize};
@@ -109,7 +109,8 @@ pub struct Contract {
     metadata: LazyOption<NFTContractMetadata>,
     // CUSTOM
     token_series_by_id: UnorderedMap<TokenSeriesId, TokenSeries>,
-    random_loot: Option<Vec<TokenSeriesId>>
+    random_loot: Option<Vec<TokenSeriesId>>,
+    last_token_by_owner: LookupMap<AccountId, TokenId>
 }
 
 const DATA_IMAGE_SVG_NEAR_ICON: &str = "data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 288 288'%3E%3Cg id='l' data-name='l'%3E%3Cpath d='M187.58,79.81l-30.1,44.69a3.2,3.2,0,0,0,4.75,4.2L191.86,103a1.2,1.2,0,0,1,2,.91v80.46a1.2,1.2,0,0,1-2.12.77L102.18,77.93A15.35,15.35,0,0,0,90.47,72.5H87.34A15.34,15.34,0,0,0,72,87.84V201.16A15.34,15.34,0,0,0,87.34,216.5h0a15.35,15.35,0,0,0,13.08-7.31l30.1-44.69a3.2,3.2,0,0,0-4.75-4.2L96.14,186a1.2,1.2,0,0,1-2-.91V104.61a1.2,1.2,0,0,1,2.12-.77l89.55,107.23a15.35,15.35,0,0,0,11.71,5.43h3.13A15.34,15.34,0,0,0,216,201.16V87.84A15.34,15.34,0,0,0,200.66,72.5h0A15.35,15.35,0,0,0,187.58,79.81Z'/%3E%3C/g%3E%3C/svg%3E";
@@ -125,6 +126,7 @@ enum StorageKey {
     TokenSeriesById,
     TokensBySeriesInner { token_series: String },
     TokensPerOwner { account_hash: Vec<u8> },
+    LastTokenByOwner,
 }
 
 #[near_bindgen]
@@ -159,7 +161,8 @@ impl Contract {
             ),
             token_series_by_id: UnorderedMap::new(StorageKey::TokenSeriesById),
             metadata: LazyOption::new(StorageKey::Metadata, Some(&metadata)),
-            random_loot: None
+            random_loot: None,
+            last_token_by_owner: LookupMap::new(StorageKey::LastTokenByOwner)
         }
     }
 
@@ -410,10 +413,15 @@ impl Contract {
             let token_id: TokenId =
                 self._nft_mint_series(target_token_series_id, receiver_id.to_string());
             NearEvent::log_nft_mint(receiver_id.to_string(), vec![token_id.clone()], None);
+            self.last_token_by_owner.insert(&receiver_id, &token_id);
             Some(token_id)
         } else {
             None
         }
+    }
+
+    pub fn get_last_token_by_owner(&self, account_id: ValidAccountId) -> TokenId {
+        self.last_token_by_owner.get(&account_id.to_string()).unwrap()
     }
 
     #[payable]
